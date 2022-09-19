68c29c858a9691c0949e7ce14baf1b8224ff4040
NgethaWachira <jngethawachira@gmail.com>
2022-09-19 08:05:58 -0700

    function thata takes a pointer and updates the value it points to

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - changes the value of the variable
+ * pointer points to 98
+ *
+ * @n: pointer to the variable to change
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
