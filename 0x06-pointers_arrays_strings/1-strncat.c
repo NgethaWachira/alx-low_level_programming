fd0ad1181414d9c2c64f9635fe23d1c6e3c05d8f
NgethaWachira <jngethawachira@gmail.com>
2022-09-21 08:06:49 -0700

    function that concatenates and bytes between strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates and find bytes among strings
+ *
+ * @dest: destination string
+ *
+ * @src: source string
+ *
+ * @n: number of bytes of sstrings to concatenate
+ *
+ * Return: pointer to the resulting string dest
+ *
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0
+	j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
