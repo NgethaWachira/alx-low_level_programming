55f439abf3b5ded9efd535283e0f1a0b7781db7c
NgethaWachira <jngethawachira@gmail.com>
2022-09-22 09:54:22 -0700

    enabled file to be xecutable

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - test for positive or negative number
+ *
+ * Return: 0 for successful execution
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_ or_negative(i);
+
+	return (0);
+}
