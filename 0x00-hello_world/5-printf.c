a8a748588bf7e6ae34c5cfe1756030481ea85154
NgethaWachira <jngethawachira@gmail.com>
2022-09-22 09:07:16 -0700

    edited the betty commenting style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 
 /**
+ * main - prints phrase like a drawing
  *
- *  * main - main block
+ * Return: 0 if executed properly
  *
- *   * Return: 0
- *    
  */
 
 int main(void)
-
 {
-
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
-
 }
